c36861196309e2826db7b0d080d3a4944f352983
Shounak Kulkarni <131899109+ShounakKulkarni@users.noreply.github.com>
2025-04-28 18:46:50 +0530

    Create tracing.bpf.c

---
+++ 1-cmd_tracing/tracing.bpf.c
@@ -0,0 +1,30 @@
+#include <vmlinux.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_core_read.h>
+#include "execsnoop.h"
+
+SEC("tracepoint/syscalls/sys_enter_execve")
+int tracepoint__syscalls__sys_enter_execve(struct trace_event_raw_sys_enter* ctx)
+{
+	u64 id;
+	pid_t pid, tgid;
+	struct task_struct *task;
+
+	uid_t uid = (u32)bpf_get_current_uid_gid();
+	id = bpf_get_current_pid_tgid();
+	tgid = id >> 32;
+
+	task = (struct task_struct*)bpf_get_current_task();
+	pid_t ppid = BPF_CORE_READ(task, real_parent, tgid);
+
+	char comm[256];
+	char *cmd_ptr = (char *)BPF_CORE_READ(ctx, args[0]);
+	bpf_probe_read_str(comm, sizeof(comm), cmd_ptr);
+
+	// Print to trace pipe
+	bpf_trace_printk("execve: PID %d PPID %d UID %d CMD %s\n", tgid, ppid, uid, comm);
+
+	return 0;
+}
+
+char LICENSE[] SEC("license") = "GPL";
